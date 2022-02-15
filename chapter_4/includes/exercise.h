334870107d9a2f495f840032d58ec9367bdcb740
Nail Ismagilov <nailismagilov555@gmail.com>
2022-02-15 21:00:40 +0100

    Modified make file. created patterns for the projects

---
+++ chapter_4/includes/exercise.h
@@ -0,0 +1,19 @@
+#ifndef __EXERCISE_H__
+#define __EXERCISE_H__
+
+void getarray(char s[], int len);
+/* getarray: stores input to an array and prints it
+ *
+ * @parameter char s[] - array where the input should be tored 
+ * @parameter int len - size of the array  */
+
+
+void print_array(char arr[]);
+/*print_array: prints array to a terminal
+ *
+ *@parameter arr - an array which should be printed
+ * */
+
+
+
+#endif
