4fa63084f447b92777b4a413e720650e976b4b17
Nail Ismagilov <nailismagilov555@gmail.com>
2022-03-02 22:19:11 +0100

    chapter 4 path

--- chapter_4/sources.mk
+++ chapter_4/sources.mk
@@ -1,7 +1,11 @@
 # Source file
-SOURCES = src/main.c \
-	  src/create_array.c \
-	  src/strrindex.c
+
+ifeq ($CHAPTER, 4)
+SOURCES = src/chapter4/main.c \
+	  src/chapter4/create_array.c \
+	  src/chapter4/strrindex.c \
+	  src/chapter4/atof.c
 
 # Include path
-INCLUDES = -I includes
+INCLUDES = -I includes/chapter4
+endif
