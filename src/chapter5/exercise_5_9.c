f4877b422a6ccdd66bdd3e5614a8dd0134e25275
Nail Ismagilov <nailismagilov555@gmail.com>
2023-03-12 20:33:57 +0100

    exercise 5-9 rewrite 2d array with point array

---
+++ src/chapter5/exercise_5_9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+char const daytab[2][13] = {{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+                                    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+    int *p;
+    leap = ((year%4 == 0 && year%100 !=0) || (year % 400 == 0));
+    p = &daytab[leap][1];
+    // Error handling
+    if (month > 12 || month == 0 || day > daytab[leap][month])
+        return 0;
+    
+    for (;month > 0; --month)
+        day += *p++;
+    return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+    int *p;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    
+    p = &daytab[leap][1];
+    //Error handling
+    if (leap && yearday <= 366 || yearday <= 365){   
+        while (yearday > *p)
+        {
+            yearday -= *p++;
+        }
+        *pday = yearday;
+        *pmonth = i;
+    }
+    printf("\n*pday: %d, *pmonth: %d\n", *pday, *pmonth);
+}
