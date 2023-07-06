f49f5bf4dbea79158b541f1b6071d4600143eb15
Nail Ismagilov <nailismagilov555@gmail.com>
2023-07-06 12:50:19 +0200

    learning argv, argc

--- src/chapter5/main.c
+++ src/chapter5/main.c
@@ -16,21 +16,51 @@ void printString (char strin[])
 }
 
 
-
+/* find: print lines that match pattern from 1st arg */
 int main(int argc, char *argv[])
 {
     char line[MAXLINE];
-    int found = 0;
+    long lineno = 0;
+    int c, except = 0, number = 0, found = 0;
 
-    if(argc != 2)
-        printf("Usage: find pattern\n");
+    while(--argc > 0 && (*++argv)[0] == '-')
+    {
+        while (c = *++argv[0])
+        {
+            switch (c){
+                case 'x':
+                    except = 1;
+                    printf("find: legal option %c\n", c);
+                    printf("find: except %d\n", number);
+                    break;
+                case 'n':
+                    number = 1;
+                    printf("find: legal option %c\n", c);
+                    printf("find: number %d\n", number);
+                    break;
+                default:
+                    printf("find: illegal option %c\n", c);
+                    argc = 0;
+                    found = -1;
+                    break;
+            }
+        }
+    }               
+
+    if(argc != 1)
+        printf("Usage: find -x -n pattern\n");
     else
         while (getlines(line, MAXLINE) > 0)
-            if (strstr(line, argv[1]) != NULL){
-                printf("%s", line);
+            lineno++;
+            if ((strstr(line, *argv) != NULL) != except){
+                if (number)
+                    printf("lineno: %ld\n", lineno);
+                printf("line: %s\n", line);
                 found++;
             }
+
     printf("Number of equality: %d", found);
-    return 0;
+
+    return found;
 }
 
